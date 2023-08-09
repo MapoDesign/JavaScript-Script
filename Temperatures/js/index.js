13c749864cd7908c2a1dda6ebff7478af25a18e6
MapoDesign <elisaresmini@gmail.com>
2023-08-09 12:30:06 +0200

    Fix Temperature functions

--- Temperatures/js/index.js
+++ Temperatures/js/index.js
@@ -1,7 +1,1 @@
-function converterF() {
-    document.getElementById('risultato').innerHTML = document.getElementById('temp').value * 1.8 + 32 + '° Fahrenheit';
-}
 
-function converterC() {
-    document.getElementById('risultato').innerHTML = (document.getElementById('temp').value - 32) / 1.8 + '° Celsius';
-}
